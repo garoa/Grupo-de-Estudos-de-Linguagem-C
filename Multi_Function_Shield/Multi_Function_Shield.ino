38a71a3a197029ffd91542ce0b2c1ce07ba6ec2e
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-06-11 11:26:28 -0300

    Arquivo inicial de sketch para Arduino fornecido pelo Marcelo Campos
    com exemplo de uso do Multi Function Shield.

---
+++ Multi_Function_Shield/Multi_Function_Shield.ino
@@ -0,0 +1,40 @@
+int LED_D1 = 13;
+int LED_D2 = 12;
+int LED_D3 = 11;
+int LED_D4 = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(A1, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A1, INPUT);
+
+  pinMode(LED_D1, OUTPUT);
+  pinMode(LED_D2, OUTPUT);
+  pinMode(LED_D3, OUTPUT);
+  pinMode(LED_D4, OUTPUT);
+
+  digitalWrite(LED_D1, HIGH);
+  digitalWrite(LED_D2, HIGH);
+  digitalWrite(LED_D3, HIGH);
+  digitalWrite(LED_D4, HIGH);
+}
+
+void loop()
+{
+  Serial.println("App Init OK");
+
+  while(1)
+  {
+    if(digitalRead(A1) == HIGH)
+    {
+      digitalWrite(LED_D1, HIGH);
+    }
+    else if(digitalRead(A1) == LOW)
+    {
+      digitalWrite(LED_D1, LOW);
+    }
+  }
+}
